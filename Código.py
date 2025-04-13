f4e73975c06384a13cd735be644554ebe0737068
Odoner07 <Odoner07@terremotolava17@gmail.com>
2025-04-13 21:44:46 +0100

    Bucle principal + selector de opción creado

--- Código.py
+++ Código.py
@@ -1,4 +1,4 @@
-import os
+import os, time
 
 def Listar_Partidas():
     # Listar los elementos de las partidas
@@ -15,4 +15,17 @@ def Listar_Partidas():
     else:
         print("No hay partidas guardadas en el directorio actual.")
 
-Listar_Partidas()
+# Bucle para seleccionar una opción en el menú
+while True:
+    print("Bienvenido a Python-RPG, que es lo que quieres hacer?: ")
+    print("1. Listar las partidas")
+    #Selector de opciones
+    Opcion = int(input("Quiero: "))
+    if Opcion == 1:
+        print("")
+        Listar_Partidas()
+        #Le dice al programa que espere 1.5 segundos
+        time.sleep(0.8)
+        print("")
+    else:
+        print("Opción no válida. Por favor, elige una opción válida.")
